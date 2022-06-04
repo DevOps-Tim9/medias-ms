b40d8de4eeafc6caf4c518a1c449b55064a3c514
Milan Marinkovic <milan_marinkovic98@hotmail.com>
2022-06-04 17:37:40 +0200

    feat: add rabbitmq

---
+++ src/rabbitmq/RMQConsumer.go
@@ -0,0 +1,85 @@
+package rabbitmq
+
+import (
+	"encoding/json"
+	"medias-ms/src/dto"
+	"medias-ms/src/service"
+
+	"github.com/streadway/amqp"
+)
+
+type RMQConsumer struct {
+	ConnectionString string
+	MediaService     service.IMediaService
+}
+
+func (r RMQConsumer) StartRabbitMQ() (*amqp.Channel, error) {
+	connectRabbitMQ, _ := amqp.Dial(r.ConnectionString)
+
+	channelRabbitMQ, _ := connectRabbitMQ.Channel()
+
+	err := channelRabbitMQ.ExchangeDeclare(
+		"DeleteImageOnMedias-MS-exchange",
+		"direct",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	queue, err := channelRabbitMQ.QueueDeclare(
+		"DeleteImageOnMedias-MS",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = channelRabbitMQ.QueueBind(
+		queue.Name,
+		"DeleteImageOnMedias-MS-routing-key",
+		"DeleteImageOnMedias-MS-exchange",
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = channelRabbitMQ.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return channelRabbitMQ, nil
+}
+
+func (r RMQConsumer) HandleDeleteImage(message []byte) {
+	var mediaDto dto.MediaDto
+
+	json.Unmarshal([]byte(message), &mediaDto)
+
+	r.MediaService.Delete(mediaDto.Id)
+}
+
+func (r RMQConsumer) Worker(messages <-chan amqp.Delivery) {
+	for delivery := range messages {
+		r.HandleDeleteImage(delivery.Body)
+	}
+}
